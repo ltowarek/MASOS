86c8ed3011276262a247c1972d8e9d0ecc8df016
Lukasz Towarek <lukasz.towarek@gmail.com>
2015-09-28 16:49:53 +0200

    Change compiler to gcc

--- 1/build.bat
+++ 1/build.bat
@@ -1,2 +1,2 @@
 mkdir build
-g++ -o build\exercise_1.exe program\exercise_1.cpp
+gcc -o build\exercise_1.exe program\exercise_1.cpp
--- 1/build.sh
+++ 1/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir build
-g++ -o build/exercise_1.exe program/exercise_1.cpp
+gcc -o build/exercise_1.exe progccram/exercise_1.cpp
 chmod +x build/exercise_1.exe
