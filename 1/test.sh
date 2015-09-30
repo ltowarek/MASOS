48315b3e83403ee94c02eb70679e8189ed7ed3f8
Lukasz Towarek <lukasz.towarek@gmail.com>
2015-09-30 19:39:26 +0200

    Update test scripts

--- 1/test.sh
+++ 1/test.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-python3.4 test/exercise_1_tester.py "build/exercise_1.exe"
+python3.4 test/exercise_1_tester.py "build/exercise_1.exe" "test/sample_input/test0" "test/sample_output/wzor0"
+python3.4 test/exercise_1_tester.py "build/exercise_1.exe" "test/sample_input/test1" "test/sample_output/wzor1"
+python3.4 test/exercise_1_tester.py "build/exercise_1.exe" "test/sample_input/test2" "test/sample_output/wzor2"
+python3.4 test/exercise_1_tester.py "build/exercise_1.exe" "test/sample_input/test3" "test/sample_output/wzor3"
+python3.4 test/exercise_1_tester.py "build/exercise_1.exe" "test/sample_input/test4" "test/sample_output/wzor4"
+python3.4 test/exercise_1_tester.py "build/exercise_1.exe" "test/sample_input/test5" "test/sample_output/wzor5"
