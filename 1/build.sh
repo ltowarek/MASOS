2232b073060de6ffd84e0943480bfe085dd716bf
Lukasz Towarek <lukasz.towarek@gmail.com>
2015-10-03 16:50:03 +0200

    Add compile.sh

--- 1/build.sh
+++ 1/build.sh
@@ -9,6 +9,5 @@ make
 make install
 cd ../..
 
-mkdir build
-g++ -I include -L lib -l gmp -o build/exercise_1.exe program/exercise_1.cpp
-chmod +x build/exercise_1.exe
+chmod +x compile.sh
+./compile.sh
