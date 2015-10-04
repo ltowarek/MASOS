50f9f456b4e3dbd1ef1e5a95cd37a9521020c86e
Lukasz Towarek <lukasz.towarek@gmail.com>
2015-10-04 13:40:10 +0200

    Fix issue with compile.sh

--- 1/compile.sh
+++ 1/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir build
-g++ -I include -L lib -l gmp -o build/exercise_1.exe program/exercise_1.cpp
+g++ program/exercise_1.cpp -I include -L lib -l gmp -o build/exercise_1.exe
 chmod +x build/exercise_1.exe
