b0f0b1d511078fc966ae1dc95afb06c730c321e1
Lukasz Towarek <lukasz.towarek@gmail.com>
2015-10-04 18:30:54 +0200

    Compile for debugging

--- 1/compile.sh
+++ 1/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir build
-g++ program/exercise_1.cpp -I include -L lib -l gmp -o build/exercise_1.exe
+g++ program/exercise_1.cpp -I include -L lib -l gmp -o build/exercise_1.exe -g
 chmod +x build/exercise_1.exe
