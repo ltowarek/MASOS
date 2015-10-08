3f5ba8ff4a739124dcb03d005f100646c556d42e
marszuka <marszuka@gmail.com>
2015-10-08 22:37:34 +0200

    Removed trailing zeros

--- 1/program/exercise_1.cpp
+++ 1/program/exercise_1.cpp
@@ -4,11 +4,30 @@
 #include <vector>
 #include <gmpxx.h>
 #include <iomanip>
+#include <sstream>
 
 static const bool is_debug = false;
 
+std::string fixTrailingZeros(mpf_class &num, int p) {
+	std::ostringstream strout;
+	strout.setf(std::ios::fixed, std::ios::floatfield);
+	strout.precision(p);
+    strout << num;
+    std::string str = strout.str();
+    size_t end = str.find_last_not_of( '0' ) + 1;
+    str.erase(end);
+	if (str[str.length() - 1] == '.') {
+		return str.erase(--end);
+	}
+	return str;
+}
+
 int main(int argc, char** argv) {
 	std::cin.sync_with_stdio(false);
+
+	//2^16 = 65536 -> from spec
+	//TODO: check if this value need to be multiplied [base(10) or base(2)] ??
+    mpf_set_default_prec(65536);
 
     if (argc != 2) {
         std::cout << "Wrong number of parameters!" << std::endl;
@@ -20,12 +39,6 @@ int main(int argc, char** argv) {
     if (is_debug) {
         std::cout << "d: " << d << std::endl;
     }
-
-    //2^16 = 65536 -> from spec
-	//TODO: check if this value need to be multiplied [base(10) or base(2)] ??
-    mpf_set_default_prec(65536);
-	std::cout.setf(std::ios::fixed, std::ios::floatfield);
-	std::cout.precision(d);
 
     // 1 <= n <= 2^24 (16777216)
     int n = 0;
@@ -57,7 +70,7 @@ int main(int argc, char** argv) {
     }
     mpq_class mean = sum / n;
 	mpf_class f_mean(mean);
-	std::cout << f_mean << std::endl;
+	std::cout << fixTrailingZeros(f_mean, d) << std::endl;
 
     // Variance
     mpq_class first;
@@ -71,7 +84,7 @@ int main(int argc, char** argv) {
     second *= second;
     mpq_class variance = first - second;
 	mpf_class f_variance(variance);
-	std::cout << f_variance << std::endl;
+	std::cout << fixTrailingZeros(f_variance, d) << std::endl;
 
     // Period
     int p = 0;
