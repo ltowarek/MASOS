d37cce01f66945744793addfdcb91f60dc1ad674
Marek Szukalski <marek.szukalski@intel.com>
2015-10-19 16:57:06 +0200

    Move gmp to the root directory.

--- 1/compile.sh
+++ 1/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir build
-g++ program/exercise_1.cpp -I include -L lib -l gmpxx -l gmp -o build/exercise_1.exe -g
+g++ program/exercise_1.cpp -I ../include -L ../lib -l gmpxx -l gmp -o build/exercise_1.exe -g
 chmod +x build/exercise_1.exe
--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 ./install_gmp.sh
+cd 1
 ./compile.sh
+cd ..
