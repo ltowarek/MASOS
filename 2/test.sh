297d1a9bfd9b923f3ee2abc7c20912d394daf74f
Lukasz Towarek <lukasz.towarek@gmail.com>
2015-10-22 20:17:51 +0200

    Change precision in exercise 2 tests

--- 2/test.sh
+++ 2/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-python3.4 test/exercise_2_tester.py "build/exercise_2.exe 9999" "test/sample_input/test1" "test/sample_output/wzor1"
-python3.4 test/exercise_2_tester.py "build/exercise_2.exe 9999" "test/sample_input/test2" "test/sample_output/wzor2"
-python3.4 test/exercise_2_tester.py "build/exercise_2.exe 9999" "test/sample_input/test3" "test/sample_output/wzor3"
-python3.4 test/exercise_2_tester.py "build/exercise_2.exe 9999" "test/sample_input/test4" "test/sample_output/wzor4"
-python3.4 test/exercise_2_tester.py "build/exercise_2.exe 9999" "test/sample_input/test5" "test/sample_output/wzor5"
+python3.4 test/exercise_2_tester.py "build/exercise_2.exe 100" "test/sample_input/test1" "test/sample_output/wzor1"
+python3.4 test/exercise_2_tester.py "build/exercise_2.exe 100" "test/sample_input/test2" "test/sample_output/wzor2"
+python3.4 test/exercise_2_tester.py "build/exercise_2.exe 100" "test/sample_input/test3" "test/sample_output/wzor3"
+python3.4 test/exercise_2_tester.py "build/exercise_2.exe 100" "test/sample_input/test4" "test/sample_output/wzor4"
+python3.4 test/exercise_2_tester.py "build/exercise_2.exe 100" "test/sample_input/test5" "test/sample_output/wzor5"
